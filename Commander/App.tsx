ed47935d7606bbab0c741d1f2294b8bf463ba44f
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-10-08 17:58:13 -0300

    Add commander page

--- Commander/App.tsx
+++ Commander/App.tsx
@@ -8,107 +8,84 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TextInput,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  const [name, setName] = useState<string>('');
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={'dark-content'} />
+      <View>
+        <Text style={[styles.title, styles.text]}>Commander APP</Text>
+        <Text style={[styles.subtitle, styles.text]}>
+          This app renders the information, generates the token and send to
+          Listener
+        </Text>
+      </View>
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Insert your name here."
+          value={name}
+          onChangeText={setName}
+          autoFocus={true}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Insert your name here."
+          value={name}
+          onChangeText={setName}
+          autoFocus={true}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Insert your name here."
+          value={name}
+          onChangeText={setName}
+          autoFocus={true}
+          style={styles.input}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  text: {
+    textAlign: 'center',
+    color: 'white',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 30,
   },
-  highlight: {
-    fontWeight: '700',
+  subtitle: {
+    fontSize: 16,
+    paddingVertical: 10,
+    paddingHorizontal: 50,
+  },
+  form: {
+    height: 180,
+    justifyContent: 'space-between',
+  },
+  input: {
+    backgroundColor: '#cdcdcd',
+    padding: 10,
+    borderRadius: 5,
+    color: 'black',
   },
 });
 
